68bedb6391044dc66e134bd31a7aa01246a96207
Jay Savlani <jay.savlani@allegiantair.com>
2024-06-07 10:23:44 +0530

    fixed header flex direction for responsive view

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,13 +3,13 @@
 	import SVELTE_LOGO from '../lib/images/svelte-logo.svg';
 </script>
 
-<header class="self-stretch flex flex-col sm:flex-row justify-between items-center">
-	<div class="flex flex-row gap-3 bg-white p-3 max-h-[10vh]">
+<header class="self-stretch flex flex-row justify-between items-center">
+	<div class="flex flex-row gap-3 bg-white p-2 lg:p-3 max-h-[10vh]">
 		<img src={LOGO} class="h-7 w-7" alt="Rubber band logo" />
 		<span class="text-2xl">FLEXBOX PLAYGROUND</span>
 	</div>
-	<div class="flex flex-row gap-2 items-center me-10 p-4 sm:p-0">
-		<img class="h-10 w-10" src={SVELTE_LOGO} alt="Svelte logo" />
-		<span class="text-lg">Powered by Svelte</span>
+	<div class="flex flex-row gap-2 items-center me-10 p-1 lg:p-4 sm:p-0">
+		<img class="h-5 w-5 lg:h-10 lg:w-10" src={SVELTE_LOGO} alt="Svelte logo" />
+		<span class="text-sm lg:text-lg">Powered by Svelte</span>
 	</div>
 </header>
